1db91c84106caac7036cfae2947e7d5cdacb5ad1
Oroghene Emudainohwo <oemudainohwo@gmail.com>
2022-05-05 09:14:59 -0400

    first compile. fixed import errors

--- contracts/UniswapExample.sol
+++ contracts/UniswapExample.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity =0.7.6;
+pragma solidity >=0.7.6;
 pragma abicoder v2;
 
-import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+// import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/contracts/libraries/TransferHelper.sol";
+import "../interfaces/ISwapRouter.sol";
 
 contract SwapExamples {
     // For the scope of these swap examples,
